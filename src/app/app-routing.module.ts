78142ce1ef85a0affc49616156cd5a1482818a82
Carlos Alexandre <37070689+carlosmoretti@users.noreply.github.com>
2023-10-18 15:31:11 -0300

    use hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,19 @@ import { SessionGuard } from './guard/session.guard';
 import { SessionComponent } from './pages/session/session.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('../app/pages/portal/portal.module').then(e => e.PortalModule), canActivate: [SessionGuard] },
-  { path: 'session', children: [
-    { path: '', component: SessionComponent },
-    { path: 'registrar/:token', component: SessionComponent, data: { registrar: true }}
-  ]},
+  { path: '', children: [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadChildren: () => import('../app/pages/portal/portal.module').then(e => e.PortalModule), canActivate: [SessionGuard] },
+    { path: 'session', children: [
+      { path: '', component: SessionComponent },
+      { path: 'registrar/:token', component: SessionComponent, data: { registrar: true }}
+    ]},
+  ]}
+  
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
